e7a6e4c16ea34c528a299180c9a3ea0c66d40661
alexCaceres123 <acaceres710@boscdelacoma.cat>
2023-04-04 19:20:59 +0200

    utils commit

--- utils.test.js
+++ utils.test.js
@@ -5,3 +5,21 @@ describe("Calculate unit", () => {
         expect(calculate(1, 2, "+")).toBe(3);
     });
 });
+
+describe("Calculate unit", () => {
+    test("Fa la resta 2-1=1", () => {
+        expect(calculate(2, 1, "-")).toBe(1);
+    });
+});
+
+describe("Calculate unit", () => {
+    test("Fa la multipliació 2*2=4", () => {
+        expect(calculate(2, 2, "*")).toBe(4);
+    });
+});
+
+describe("Calculate unit", () => {
+    test("Fa la divisió 2/2=1", () => {
+        expect(calculate(2, 2, "/")).toBe(1);
+    });
+});
