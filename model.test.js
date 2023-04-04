eededd7d5efb317eac05f6243339d68a2dd82153
alexCaceres123 <acaceres710@boscdelacoma.cat>
2023-04-04 19:34:14 +0200

    model update

--- model.test.js
+++ model.test.js
@@ -10,3 +10,31 @@ describe("Calculadora integration", () => {
         expect(result).toBe(3)
     });
 });
+
+describe("Operació one value", () => {
+    test("Veure si nomes hi ha un valor, retornar-lo: 1", () => {
+        const calc = new Calculadora()
+        calc.set("1")
+        const result = calc.operate()
+        expect(result).toBe("1")
+    });
+});
+
+describe("Operació, not operator", () => {
+    test("No operador, retornar segon valor: 12", () => {
+        const calc = new Calculadora()
+        calc.set("1")
+        calc.set("")
+        calc.set("2")
+        const result = calc.operate()
+        expect(result).toBe("12")
+    });
+});
+
+describe("Operació, undefined", () => {
+    test("Undefined value, retornar: Error", () => {
+        const calc = new Calculadora()
+        const result = calc.operate()
+        expect(result).toBe("Error")
+    });
+});
